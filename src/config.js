ac98f7055a03bfedc3d66376535a9e324ee062a2
Emre Ayvaz <eayvaz99@gmail.com>
2023-08-09 14:59:22 +0300

    new config added

--- src/config.js
+++ src/config.js
@@ -13,19 +13,41 @@ const saimConfig = {
   },
 };
 
-const emreConfig = {
+const emreConfigMySpa = {
   oidc: {
     issuer: "https://dev-94183903.okta.com/oauth2/default",
     clientId: "0oaaq9zbixMJxCKff5d7",
     scopes: ["openid", "profile", "email"],
     redirectUri: `${window.location.origin}/login/callback`,
+    pkce: true,
+    // disableHttpsCheck: true,
   },
   widget: {
     issuer: "https://dev-94183903.okta.com/oauth2/default",
     clientId: "0oaaq9zbixMJxCKff5d7",
     redirectUri: `${window.location.origin}/login/callback`,
     scopes: ["openid", "profile", "email"],
+    pkce: true,
+    // disableHttpsCheck: true,
+  },
+};
+const emreConfigMySpa2 = {
+  oidc: {
+    issuer: "https://dev-94183903.okta.com/oauth2/default",
+    clientId: "0oaaqsq0vuEfcsn1g5d7",
+    scopes: ["openid", "profile", "email"],
+    redirectUri: `${window.location.origin}/login/callback`,
+    pkce: true,
+    // disableHttpsCheck: true,
+  },
+  widget: {
+    issuer: "https://dev-94183903.okta.com/oauth2/default",
+    clientId: "0oaaqsq0vuEfcsn1g5d7",
+    redirectUri: `${window.location.origin}/login/callback`,
+    scopes: ["openid", "profile", "email"],
+    pkce: true,
+    // disableHttpsCheck: true,
   },
 };
 
-export default emreConfig;
+export default emreConfigMySpa;

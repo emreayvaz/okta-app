5ee7c0a63f04d30e683f9567620f1afab4b55733
Emre Ayvaz <eayvaz99@gmail.com>
2023-08-10 12:37:04 +0300

    request to backend for user login

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
+import axios from "axios";
+
+const extCallback = async (idToken,email,fullname) => {
+  return await axios.post("https://localhost:7001/tr/account/okta-login-register",{
+    idToken:idToken,
+    email:email,
+    fullName:fullname
+  },
+  {
+    headers:{
+      "Accept" : "application/json",
+      "Content-Type" : "application/json",
+      "Access-Control-Allow-Origin" : "*",
+    }
+  })
+}
 
 const Home = () => {
   const { oktaAuth, authState } = useOktaAuth();
@@ -14,10 +30,13 @@ const Home = () => {
 
   const handleLogout = async () => oktaAuth.signOut();
 
+  const handleRequest = async () => extCallback(authState.accessToken.accessToken, authState.idToken.claims.email, authState.idToken.claims.name).then((resp)=>console.log(resp.data));
+
   return (
     <div id="home">
         <Link to="/">Home</Link> | &nbsp;
         <Link id="protected" to="/protected">Protected</Link> | &nbsp;
+        {authState.isAuthenticated && <button id="logout-button" type="button" onClick={handleRequest}>İstek At</button> } | &nbsp;
         {
           authState.isAuthenticated
             ? <button id="logout-button" type="button" onClick={handleLogout}>Logout</button>
--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import OktaSignInWidget from './OktaSignInWidget';
 import { useOktaAuth } from '@okta/okta-react';
 
+
+
 const Login = ({ config }) => {
   const { oktaAuth, authState } = useOktaAuth();
   const navigate = useNavigate();
-  const onSuccess = (tokens) => {
-    oktaAuth.handleLoginRedirect(tokens);
+  const onSuccess = async (tokens) => {
+    await oktaAuth.handleLoginRedirect(tokens).then(()=>{
+      var token = tokens.idToken.idToken;
+      var claims = tokens.idToken.claims;
+      // extCallback(token,claims.name, claims.email).then((resp)=>console.log(resp.data));
+      // console.log(token)
+      // console.log(claims.name)
+      // console.log(claims.email)
+    });
+    // await extCallback(authState.idToken.idToken,authState.idToken.claims.name, authState.idToken.claims.email).then((resp)=>console.log(resp.data))
   };
+
 
   const onError = (err) => {
     console.log('Sign in error:', err);
@@ -17,10 +28,12 @@ const Login = ({ config }) => {
   if (!authState) {
     return <div>Loading ... </div>;
   }
-
-  return authState.isAuthenticated ?
-    navigate("/") :
-    <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>;
+  
+  return (
+    // authState.isAuthenticated ?
+    //   navigate("/") :
+      <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>
+  )
 };
 
 export default Login;
